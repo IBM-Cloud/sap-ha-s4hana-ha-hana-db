62d0cb439916b507b3a73e73b8d31a5133c74098
Marian Ropota <marian.ropota2@ibm.com>
2022-09-30 12:12:55 +0300

    Fixing code removal

--- cli/modules/pg/output.tf
+++
@@ -1,3 +0,0 @@
-output "PLACEMENT_GROUP" {
-  value		= ibm_is_placement_group.sapha-pg.name
-}
--- cli/modules/pg/pl-gr.tf
+++
@@ -1,9 +0,0 @@
-data "ibm_resource_group" "group" {
-  name		= var.RESOURCE_GROUP
-}
-
-resource "ibm_is_placement_group" "sapha-pg" {
-  strategy = "power_spread"
-  name     = lower ("sapha-${var.SAP_SID}")
-  resource_group = data.ibm_resource_group.group.id
-}
